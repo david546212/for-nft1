c6bee656b48ba1dc2c5eeb9e191558299866cd22
david54621 <88965600+david54621@users.noreply.github.com>
2022-05-17 14:16:20 +0200

    ad

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['for-nft.herokuapp.com', 'for-nft.com']
+ALLOWED_HOSTS = ['for-nft.herokuapp.com', 'for-nft.com' '127.0.0.1:8000']
 
 
 # Application definition
